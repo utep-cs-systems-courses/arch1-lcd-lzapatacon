d5dc7812fd236afca7d78987ea8395fef7b9ce8c
Leonardo <lzapatacon@miners.utep.com>
2021-11-28 09:57:04 -0700

    Added vertical character methods

--- lcdLib/lcddraw.h
+++ lcdLib/lcddraw.h
@@ -60,6 +60,26 @@ void drawChar5x7(u_char col, u_char row, char c,
  */
 void drawRectOutline(u_char colMin, u_char rowMin, u_char width, u_char height,
 		     u_int colorBGR);
+/** Vertical character
+ *  5x7 font - this function draws background pixels
+ *  Adapted from RobG's EduKit
+ */
+void drawVerChar5x7(u_char rcol, u_char rrow, char c, 
+		    u_int fgColorBGR, u_int bgColorBGR);
+/** Draw string at col,row vertically
+ *  Type:
+ *  FONT_SM - small (5x8,) FONT_MD - medium (8x12,) FONT_LG - large (11x16)
+ *  FONT_SM_BKG, FONT_MD_BKG, FONT_LG_BKG - as above, but with background color
+ *  Adapted from RobG's EduKit
+ *
+ *  \param col Column to start drawing string
+ *  \param row Row to start drawing string
+ *  \param string The string
+ *  \param fgColorBGR Foreground color in BGR
+ *  \param bgColorBGR Background color in BGR
+ */
+void drawVerString5x7(u_char col, u_char row, char *string,
+		      u_int fgColorBGR, u_int bgColorBGR);
 #endif // included
 
 
